b222830b65e79068746a0ea02463324a95bf8dbd
“manhdl” <manhdl0110@gmail.com>
2024-06-22 08:00:42 +0700

    apply new url backend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae2d5284dbc8b469d89e7ea645d1695a-543853471.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
